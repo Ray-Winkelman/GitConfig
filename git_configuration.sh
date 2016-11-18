abb0ee49ae9769314d7bd02e5496222b9ca49662
Ray Winkelman <raywinkelman@gmail.com>
2016-11-18 10:49:14 -0400

    Add merge tool

--- git_configuration.sh
+++ git_configuration.sh
@@ -46,8 +46,11 @@ git config --system merge.renamelimit 99999
 
 
 # Windows Only
-#git config --system merge.tool p4merge
-#git config --system mergetool.p4merge.cmd 'p4merge.exe \"$BASE\" \"$LOCAL\" \"$REMOTE\" \"$MERGED\"'
+git config --global merge.tool p4merge
+git config --global mergetool.p4merge.cmd p4merge '$BASE $LOCAL $REMOTE $MERGED'
+git config --global mergetool.p4merge.trustExitCode false
+git config --global diff.tool p4merge
+git config --global difftool.p4merge.cmd p4merge '$LOCAL $REMOTE'
 
 # Repo Specific
 #git update-index --assume-unchanged file.ext
