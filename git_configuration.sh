7d9dc1ab2412f934cd63761382fa7c8b079e0a6e
Ray Winkelman <raywinkelman@gmail.com>
2017-06-13 02:14:25 -0300

    Changed over to global settings.

--- git_configuration.sh
+++ git_configuration.sh
@@ -3,51 +3,51 @@
 tput rmam
 git config --global user.email "raywinkelman@gmail.com"
 git config --global user.name "Ray Winkelman"
-git config --system alias.l "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
-git config --system alias.a "add"
-git config --system alias.aa "add --all"
-git config --system alias.cm "commit -m"
-git config --system alias.cam \
+git config --global alias.l "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global alias.a "add"
+git config --global alias.aa "add --all"
+git config --global alias.cm "commit -m"
+git config --global alias.cam \
 '!git add --all && git commit -m'
-git config --system alias.rewind "reset --hard HEAD~1"
-git config --system alias.st "status -s -b -uall"
-git config --system alias.cl "clean -dfx"
-git config --system alias.ch "checkout -f"
-git config --system alias.cha "checkout -f -- ."
-git config --system alias.pr "pull --rebase"
-git config --system alias.cp "cherry-pick"
-git config --system alias.mp "merge -Xpatience -Xdiff-algorithm=patience -Xfind-renames -Xignore-space-change"
-git config --system alias.mo "merge -s ours"
-git config --system alias.show-hidden "ls-files -v | grep ^[a-z]"
-git config --system alias.gcp "gc --aggressive --prune --force"
-git config --system alias.commend 'commit --amend --no-edit'
-git config --system alias.it \
+git config --global alias.rewind "reset --hard HEAD~1"
+git config --global alias.st "status -s -b -uall"
+git config --global alias.cl "clean -dfx"
+git config --global alias.ch "checkout -f"
+git config --global alias.cha "checkout -f -- ."
+git config --global alias.pr "pull --rebase"
+git config --global alias.cp "cherry-pick"
+git config --global alias.mp "merge -Xpatience -Xdiff-algorithm=patience -Xfind-renames -Xignore-space-change"
+git config --global alias.mo "merge -s ours"
+git config --global alias.show-hidden "ls-files -v | grep ^[a-z]"
+git config --global alias.gcp "gc --aggressive --prune --force"
+git config --global alias.commend 'commit --amend --no-edit'
+git config --global alias.it \
 '!git init && git commit -m "root" --allow-empty'
-git config --system alias.stsh 'stash --all'
-git config --system core.filemode false
-git config --system color.ui always
-git config --system color.branch always
-git config --system color.diff always
-git config --system color.interactive always
-git config --system color.status always
-git config --system color.grep always
-git config --system color.pager true
-git config --system color.decorate always
-git config --system color.showbranch always
-git config --system core.preloadindex true
-git config --system core.ignorecase false
+git config --global alias.stsh 'stash --all'
+git config --global core.filemode false
+git config --global color.ui always
+git config --global color.branch always
+git config --global color.diff always
+git config --global color.interactive always
+git config --global color.status always
+git config --global color.grep always
+git config --global color.pager true
+git config --global color.decorate always
+git config --global color.showbranch always
+git config --global core.preloadindex true
+git config --global core.ignorecase false
 git config --global core.safecrlf false
-git config --system advice.pushNonFastForward false
-git config --system advice.statusHints false
-git config --system diff.algorithm patience
-git config --system diff.mnemonicprefix true
-git config --system diff.renames copies
-git config --system push.default tracking
-git config --system rerere.enabled false
-git config --system merge.stat false
-git config --system merge.renamelimit 99999 
-git config --system http.sslVerify false
-git config --system http.sslCAinfo /bin/curl-ca-bundle.crt
+git config --global advice.pushNonFastForward false
+git config --global advice.statusHints false
+git config --global diff.algorithm patience
+git config --global diff.mnemonicprefix true
+git config --global diff.renames copies
+git config --global push.default tracking
+git config --global rerere.enabled false
+git config --global merge.stat false
+git config --global merge.renamelimit 999
+git config --global http.sslVerify false
+git config --global http.sslCAinfo /bin/curl-ca-bundle.crt
 
 # Windows Only
 git config --global merge.tool p4merge
